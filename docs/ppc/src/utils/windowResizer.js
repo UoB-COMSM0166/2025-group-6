7deab0d6c09b85a781dd1a0e8d90b22d837dff07
Nilay Bhaisare <nilaybhaisare1098@gmail.com>
2025-02-28 13:00:30 +0000

    Added Exit button to Game screen.

--- docs/ppc/src/utils/windowResizer.js
+++ docs/ppc/src/utils/windowResizer.js
@@ -20,7 +20,7 @@ export function updateDimensions() {
     game.board.goalPost.goalHeight = height * 0.25;
     game.board.goalPost.goalY = height / 2 ;
     game.scoreBoard.update();
-    game.gamePage.pauseButton.reset();
+    game.gamePage.reset();
     game.landingPage.reset();
 }
 
