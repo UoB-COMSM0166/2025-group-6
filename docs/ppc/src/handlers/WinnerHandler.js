8c454aaef73577eeb23ecdb5c8272b90dba25ae0
adwaith911 <adwaithssundar04@gmail.com>
2025-03-03 22:43:24 +0000

    change win threshold

--- docs/ppc/src/handlers/WinnerHandler.js
+++ docs/ppc/src/handlers/WinnerHandler.js
@@ -2,7 +2,7 @@
 export default class WinnerHandler {
     constructor(game) {
         this.game = game;
-        this.winningScore = 2; // Default score needed to win
+        this.winningScore = 10; // Default score needed to win
         this.winner = null; // Will be 1 or 2 when someone wins
     }
 
