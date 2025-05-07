0a2124238fed5207d8827c7ea1d1cfb67bbc7e3b
adwaith911 <adwaithssundar04@gmail.com>
2025-05-07 15:54:15 +0100

    fix resize issue

--- docs/ppc/src/utils/windowResizer.js
+++ docs/ppc/src/utils/windowResizer.js
@@ -5,8 +5,13 @@ import { constants } from "../core/config.js";
 /* Function to handle window resize event
 -p5.js function */
 function windowResized() {
+  const laptopWidth = 1366;
+  const laptopHeight = 768;
+  // Only resize if current dimensions are smaller than laptop dimensions
+  if (windowWidth < laptopWidth || windowHeight < laptopHeight) {
     resizeCanvas(windowWidth, windowHeight);
     updateDimensions();
+  }
 }
 
 /* Function to update the game elements whenever browser windows is resized
